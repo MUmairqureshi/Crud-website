1527270f752085b07f9633f09316fba0e66e72e2
MUmairqureshi <umairqadeer586@gmail.com>
2022-06-19 14:53:20 -1200

    all

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// import ReactDOM from 'react-dom';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<AppRouter />
   </React.StrictMode>
 );
 
